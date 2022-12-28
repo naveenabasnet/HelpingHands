6be8f57c21748abca9bae53dbb2044a0d1038db2
naveenabasnet <nabinabasnet21123@gmail.com>
2022-12-28 15:07:42 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HelpingHands
+# HelpingHands
---
+++ src/main/java/com/nabina/configuration/HelpingHandsAppInitialzier.java
@@ -0,0 +1,8 @@
+package com.nabina.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class HelpingHandsAppInitialzier extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	
+}
